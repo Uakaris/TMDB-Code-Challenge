33ba3df3871739129c8dd9171e457762b7e7bb76
Charlie Jay <charliejbjay@gmail.com>
2024-09-16 13:03:04 +1000

    Added .env

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,21 +1,29 @@
+const API_READ_ACCESS_TOKEN = import.meta.env.VITE_API_READ_ACCESS_TOKEN;
+
+console.log(import.meta.env);
+
+
 const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjUwZjM2NDBlYzYwODgyZTdjMGMxOTU1MGY2NzViMiIsIm5iZiI6MTcyMzE4OTY4NC41MjM1OTgsInN1YiI6IjY2YjVjOTE4MzgyN2NjM2QxY2Q3YzU0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l5hA_pIA6dSHwKQoxjtYC9DYthtJ67zaAVzzg5YI_PY'
-    }
-  };
+        accept: "application/json",
+        Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
+    },
+};
 
 const fetchPopularMovies = async () => {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/movie/popular', options);
+        const response = await fetch(
+            "https://api.themoviedb.org/3/movie/popular",
+            options
+        );
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Failed to fetch popular movies', error);
+        console.error("Failed to fetch popular movies", error);
     }
 };
 
-fetchPopularMovies().then(data => console.log(data));
+// fetchPopularMovies().then((data) => console.log(data));
 
 export { fetchPopularMovies };
